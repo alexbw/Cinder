254a7ae5d9dbef342f1383aa3233a2a63138de89
Alex Wiltschko <alex.bw@gmail.com>
2010-06-08 16:22:58 -0700

    Switched accelerometer handling over to a separate class. This is in an effort to allow the embedding of Cinder into a plain jane Cocoa Touch app, which means it can't assume the app delegate will play nice with Cinder's accelerometer methods.

--- include/cinder/app/AppCocoaTouch.h
+++ include/cinder/app/AppCocoaTouch.h
@@ -1,15 +1,15 @@
 /*
  Copyright (c) 2010, The Barbarian Group
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
-
+ 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
@@ -18,7 +18,7 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #pragma once
 
@@ -27,129 +27,129 @@
 #include "cinder/app/TouchEvent.h"
 #include "cinder/app/AccelEvent.h"
 
-
 namespace cinder { namespace app {
-
-struct AppCocoaTouchState;
-
-class AppCocoaTouch : public App {
-  public:
-	class Settings : public App::Settings {
-	  public:
-		Settings()
+	
+	struct AppCocoaTouchState;
+	
+	class AppCocoaTouch : public App {
+	public:
+		class Settings : public App::Settings {
+		public:
+			Settings()
 			: App::Settings(), mEnableMultiTouch( true ) {}
-
-		//! Registers the app to receive multiTouch events from the operating system. Enabled by default. If disabled, touch events are mapped to mouse events.
-		void		enableMultiTouch( bool enable = true ) { mEnableMultiTouch = enable; }
-		//! Returns whether the app is registered to receive multiTouch events from the operating system. Enabled by default. If disabled, touch events are mapped to mouse events.
-		bool		isMultiTouchEnabled() const { return mEnableMultiTouch; }
+			
+			//! Registers the app to receive multiTouch events from the operating system. Enabled by default. If disabled, touch events are mapped to mouse events.
+			void		enableMultiTouch( bool enable = true ) { mEnableMultiTouch = enable; }
+			//! Returns whether the app is registered to receive multiTouch events from the operating system. Enabled by default. If disabled, touch events are mapped to mouse events.
+			bool		isMultiTouchEnabled() const { return mEnableMultiTouch; }
+			
+		private:
+			bool		mEnableMultiTouch;
+		};
 		
-	  private:
-		bool		mEnableMultiTouch;
+		AppCocoaTouch();
+		virtual ~AppCocoaTouch() {}
+		
+		virtual void		prepareSettings( Settings *settings ) {}
+		
+		//! Override to respond to the beginning of a multitouch sequence
+		virtual void		touchesBegan( TouchEvent event ) {}
+		//! Override to respond to movement (drags) during a multitouch sequence
+		virtual void		touchesMoved( TouchEvent event ) {}
+		//! Override to respond to the end of a multitouch sequence
+		virtual void		touchesEnded( TouchEvent event ) {}
+		//! Returns a std::vector of all active touches
+		
+		//! Returns a Vec3d of the acceleration direction
+		virtual void		accelerated( AccelEvent event ) {}
+		
+		
+		const std::vector<TouchEvent::Touch>&	getActiveTouches() const { return mActiveTouches; }
+		
+		//! Returns the width of the App's window measured in pixels, or the screen when in full-screen mode.	
+		virtual int		getWindowWidth() const;
+		//! Returns the height of the App's window measured in pixels, or the screen when in full-screen mode.	
+		virtual int		getWindowHeight() const;
+		//! Ignored on the iPhone.
+		void			setWindowWidth( int windowWidth ) {}
+		//! Ignored on the iPhone.
+		void			setWindowHeight( int windowHeight ) {}
+		//! Ignored on the iPhone.
+		void			setWindowSize( int windowWidth, int windowHeight ) {}
+		
+		//! Enables the device's accelerometer and modifies its filtering. \a updateFrequency represents the frequency with which accelerated() is called, measured in Hz. \a filterFactor represents the amount to weight the current value relative to the previous.
+		void enableAccelerometer( float updateFrequency = 30.0f, float filterFactor = 0.1f );
+		//! Turns off the accelerometer
+		void disableAccelerometer();
+		
+		//! Returns the maximum frame-rate the App will attempt to maintain.
+		virtual float		getFrameRate() const;
+		//! Sets the maximum frame-rate the App will attempt to maintain.
+		virtual void		setFrameRate( float aFrameRate );
+		//! Returns whether the App is in full-screen mode or not.
+		virtual bool		isFullScreen() const;
+		//! Sets whether the active App is in full-screen mode based on \a fullScreen
+		virtual void		setFullScreen( bool aFullScreen );
+		
+		//! Returns the number seconds which have elapsed since the active App launched.
+		virtual double		getElapsedSeconds() const;
+		
+		//! Returns the path to the application on disk
+		virtual std::string			getAppPath();
+		
+		//! Ceases execution of the application. Not implemented yet on iPhone
+		virtual void	quit();
+		
+		//! Returns a pointer to the current global AppBasic
+		static AppCocoaTouch*	get() { return sInstance; }
+		//! Returns a pointer to the current global AppBasic
+		virtual const Settings&	getSettings() const { return mSettings; }
+		
+		
+		//! \cond
+		// These are called by application instantation macros and are only used in the launch process
+		static void		prepareLaunch() { App::prepareLaunch(); }
+		static void		executeLaunch( AppCocoaTouch *app, class Renderer *renderer, const char *title, int argc, char * const argv[] ) { sInstance = app; App::executeLaunch( app, renderer, title, argc, argv ); }
+		static void		cleanupLaunch() { App::cleanupLaunch(); }
+		
+		virtual void	launch( const char *title, int argc, char * const argv[] );
+		//! \endcond
+		
+		
+		// DO NOT CALL - should be private but aren't for esoteric reasons
+		//! \cond
+		// Internal handlers - these are called into by AppImpl's. If you are calling one of these, you have likely strayed far off the path.
+		void		privatePrepareSettings__();
+		void		privateTouchesBegan__( const TouchEvent &event );
+		void		privateTouchesMoved__( const TouchEvent &event );
+		void		privateTouchesEnded__( const TouchEvent &event );
+		void		privateSetActiveTouches__( const std::vector<TouchEvent::Touch> &touches ) { mActiveTouches = touches; }
+		void		privateAccelerated__( const Vec3f &direction );
+		//! \endcond
+		
+	private:
+		friend void		setupCocoaTouchWindow( AppCocoaTouch *app );
+		
+		// The state is contained in a struct in order to avoid this .h needing to be compiled as Objective-C++
+		shared_ptr<AppCocoaTouchState>		mState;
+		
+		static AppCocoaTouch	*sInstance;	
+		Settings				mSettings;
+		
+		std::vector<TouchEvent::Touch>	mActiveTouches;
+		
+		float					mAccelFilterFactor;
+		Vec3f					mLastAccel, mLastRawAccel;
 	};
-
-	AppCocoaTouch();
-	virtual ~AppCocoaTouch() {}
-
-	virtual void		prepareSettings( Settings *settings ) {}
-
-	//! Override to respond to the beginning of a multitouch sequence
-	virtual void		touchesBegan( TouchEvent event ) {}
-	//! Override to respond to movement (drags) during a multitouch sequence
-	virtual void		touchesMoved( TouchEvent event ) {}
-	//! Override to respond to the end of a multitouch sequence
-	virtual void		touchesEnded( TouchEvent event ) {}
-	//! Returns a std::vector of all active touches
 	
-	//! Returns a Vec3d of the acceleration direction
-	virtual void		accelerated( AccelEvent event ) {}
-	
-	
-	const std::vector<TouchEvent::Touch>&	getActiveTouches() const { return mActiveTouches; }
-	
-	//! Returns the width of the App's window measured in pixels, or the screen when in full-screen mode.	
-	virtual int		getWindowWidth() const;
-	//! Returns the height of the App's window measured in pixels, or the screen when in full-screen mode.	
-	virtual int		getWindowHeight() const;
-	//! Ignored on the iPhone.
-	void			setWindowWidth( int windowWidth ) {}
-	//! Ignored on the iPhone.
-	void			setWindowHeight( int windowHeight ) {}
-	//! Ignored on the iPhone.
-	void			setWindowSize( int windowWidth, int windowHeight ) {}
-
-	//! Enables the device's accelerometer and modifies its filtering. \a updateFrequency represents the frequency with which accelerated() is called, measured in Hz. \a filterFactor represents the amount to weight the current value relative to the previous.
-	void enableAccelerometer( float updateFrequency = 30.0f, float filterFactor = 0.1f );
-	//! Turns off the accelerometer
-	void disableAccelerometer();
-	
-	//! Returns the maximum frame-rate the App will attempt to maintain.
-	virtual float		getFrameRate() const;
-	//! Sets the maximum frame-rate the App will attempt to maintain.
-	virtual void		setFrameRate( float aFrameRate );
-	//! Returns whether the App is in full-screen mode or not.
-	virtual bool		isFullScreen() const;
-	//! Sets whether the active App is in full-screen mode based on \a fullScreen
-	virtual void		setFullScreen( bool aFullScreen );
-
-	//! Returns the number seconds which have elapsed since the active App launched.
-	virtual double		getElapsedSeconds() const;
-
-	//! Returns the path to the application on disk
-	virtual std::string			getAppPath();
-
-	//! Ceases execution of the application. Not implemented yet on iPhone
-	virtual void	quit();
-
-	//! Returns a pointer to the current global AppBasic
-	static AppCocoaTouch*	get() { return sInstance; }
-	//! Returns a pointer to the current global AppBasic
-	virtual const Settings&	getSettings() const { return mSettings; }
-
-
-	//! \cond
-	// These are called by application instantation macros and are only used in the launch process
-	static void		prepareLaunch() { App::prepareLaunch(); }
-	static void		executeLaunch( AppCocoaTouch *app, class Renderer *renderer, const char *title, int argc, char * const argv[] ) { sInstance = app; App::executeLaunch( app, renderer, title, argc, argv ); }
-	static void		cleanupLaunch() { App::cleanupLaunch(); }
-	
-	virtual void	launch( const char *title, int argc, char * const argv[] );
-	//! \endcond
-
-	// DO NOT CALL - should be private but aren't for esoteric reasons
-	//! \cond
-	// Internal handlers - these are called into by AppImpl's. If you are calling one of these, you have likely strayed far off the path.
-	void		privatePrepareSettings__();
-	void		privateTouchesBegan__( const TouchEvent &event );
-	void		privateTouchesMoved__( const TouchEvent &event );
-	void		privateTouchesEnded__( const TouchEvent &event );
-	void		privateSetActiveTouches__( const std::vector<TouchEvent::Touch> &touches ) { mActiveTouches = touches; }
-	void		privateAccelerated__( const Vec3f &direction );
-	//! \endcond
-
-  private:
-	friend void		setupCocoaTouchWindow( AppCocoaTouch *app );
-	
-	// The state is contained in a struct in order to avoid this .h needing to be compiled as Objective-C++
-	shared_ptr<AppCocoaTouchState>		mState;
-	
-	static AppCocoaTouch	*sInstance;	
-	Settings				mSettings;
-	
-	std::vector<TouchEvent::Touch>	mActiveTouches;
-
-	float					mAccelFilterFactor;
-	Vec3f					mLastAccel, mLastRawAccel;
-};
-
 } } // namespace cinder::app
 
 #define CINDER_APP_COCOA_TOUCH( APP, RENDERER )								\
 int main( int argc, char *argv[] ) {										\
-	cinder::app::AppCocoaTouch::prepareLaunch();							\
-	cinder::app::AppCocoaTouch *app = new APP;								\
-	cinder::app::Renderer *ren = new RENDERER;								\
-	cinder::app::AppCocoaTouch::executeLaunch( app, ren, #APP, argc, argv );\
-	cinder::app::AppCocoaTouch::cleanupLaunch();							\
-    return 0;																\
+cinder::app::AppCocoaTouch::prepareLaunch();							\
+cinder::app::AppCocoaTouch *app = new APP;								\
+cinder::app::Renderer *ren = new RENDERER;								\
+cinder::app::AppCocoaTouch::executeLaunch( app, ren, #APP, argc, argv );\
+cinder::app::AppCocoaTouch::cleanupLaunch();							\
+return 0;																\
 }
--- src/cinder/app/AppCocoaTouch.mm
+++ src/cinder/app/AppCocoaTouch.mm
@@ -1,15 +1,15 @@
 /*
  Copyright (c) 2010, The Barbarian Group
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
-
+ 
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
@@ -18,43 +18,75 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #include "cinder/app/AppCocoaTouch.h"
 #include "cinder/app/CinderViewCocoaTouch.h"
 #include "cinder/cocoa/CinderCocoaTouch.h"
 
 namespace cinder { namespace app {
-
-AppCocoaTouch*				AppCocoaTouch::sInstance = 0;
-
-// This struct serves as a compile firewall for maintaining AppCocoaTouch state information
-struct AppCocoaTouchState {
-	CinderViewCocoaTouch		*mCinderView;
-	UIWindow					*mWindow;
-	CFAbsoluteTime				mStartTime;
-};
-
-void setupCocoaTouchWindow( AppCocoaTouch *app )
-{
-	app->privatePrepareSettings__();
-	app->mState->mWindow = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	app->mState->mCinderView = [[CinderViewCocoaTouch alloc] initWithFrame:[[UIScreen mainScreen] bounds] app:app renderer:app->getRenderer()];
-	[app->mState->mWindow addSubview:app->mState->mCinderView];
-	[app->mState->mCinderView release];
-	[app->mState->mWindow makeKeyAndVisible];
-
-	app->privateSetup__();
 	
-	[app->mState->mCinderView startAnimation];
-}
-
+	AppCocoaTouch*				AppCocoaTouch::sInstance = 0;
+	
+	// This struct serves as a compile firewall for maintaining AppCocoaTouch state information
+	struct AppCocoaTouchState {
+		CinderViewCocoaTouch		*mCinderView;
+		UIWindow					*mWindow;
+		CFAbsoluteTime				mStartTime;
+	};
+	
+	void setupCocoaTouchWindow( AppCocoaTouch *app )
+	{
+		app->privatePrepareSettings__();
+		app->mState->mWindow = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+		app->mState->mCinderView = [[CinderViewCocoaTouch alloc] initWithFrame:[[UIScreen mainScreen] bounds] app:app renderer:app->getRenderer()];
+		[app->mState->mWindow addSubview:app->mState->mCinderView];
+		[app->mState->mCinderView release];
+		[app->mState->mWindow makeKeyAndVisible];
+		
+		app->privateSetup__();
+		
+		[app->mState->mCinderView startAnimation];
+	}
+	
 } } // namespace cinder::app
 
-@interface CinderAppDelegateIPhone : NSObject <UIApplicationDelegate, UIAccelerometerDelegate> {
+@interface CinderAccelerometerDelegateIPhone : NSObject <UIAccelerometerDelegate> {
+	cinder::app::AppCocoaTouch *app;
+}
+- (id)initWithApp:(cinder::app::AppCocoaTouch *)thisApp;
+@end
+
+@implementation CinderAccelerometerDelegateIPhone
+
+
+- (id)initWithApp:(cinder::app::AppCocoaTouch *)thisApp
+{
+	if (self = [super init]) {
+		app = thisApp;
+		return self;
+	}
+	
+	return nil;
+}
+
+- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)thisAcceleration {
+	// Massage the UIAcceleration class into a Vec3f
+	ci::Vec3f direction( thisAcceleration.x, thisAcceleration.y, thisAcceleration.z );
+	app->privateAccelerated__( direction );
+}
+
+- (void)dealloc 
+{
+	[super dealloc];
+}
+
+@end
+
+@interface CinderAppDelegateIPhone : NSObject <UIApplicationDelegate> {
 	cinder::app::AppCocoaTouch	*app;
-//    UIWindow				*window;
-//    CinderViewCocoaTouch	*cinderView;
+	//    UIWindow				*window;
+	//    CinderViewCocoaTouch	*cinderView;
 }
 @end
 
@@ -69,30 +101,24 @@ void setupCocoaTouchWindow( AppCocoaTouch *app )
 
 - (void) applicationWillResignActive:(UIApplication *)application
 {
-//	[cinderView stopAnimation];
+	//	[cinderView stopAnimation];
 }
 
 - (void) applicationDidBecomeActive:(UIApplication *)application
 {
-//	[cinderView startAnimation];
+	//	[cinderView startAnimation];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-//	[cinderView stopAnimation];
+	//	[cinderView stopAnimation];
 	app->privateShutdown__();
-}
-
-- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)thisAcceleration {
-	// Massage the UIAcceleration class into a Vec3f
-	ci::Vec3f direction( thisAcceleration.x, thisAcceleration.y, thisAcceleration.z );
-	app->privateAccelerated__( direction );
 }
 
 - (void) dealloc
 {
-//	[window release];
-//	[cinderView release];
+	//	[window release];
+	//	[cinderView release];
 	
 	[super dealloc];
 }
@@ -101,114 +127,116 @@ void setupCocoaTouchWindow( AppCocoaTouch *app )
 
 
 namespace cinder { namespace app {
-
-AppCocoaTouch::AppCocoaTouch()
+	
+	AppCocoaTouch::AppCocoaTouch()
 	: App()
-{
-	mState = shared_ptr<AppCocoaTouchState>( new AppCocoaTouchState() );
-	mState->mStartTime = ::CFAbsoluteTimeGetCurrent();
-	mLastAccel = mLastRawAccel = Vec3f::zero();
-}
-
-void AppCocoaTouch::launch( const char *title, int argc, char * const argv[] )
-{
-	::UIApplicationMain( argc, const_cast<char**>( argv ), nil, @"CinderAppDelegateIPhone" );
-}
-
-int	AppCocoaTouch::getWindowWidth() const
-{
-	::CGRect bounds = [mState->mCinderView bounds];
-	return ::CGRectGetWidth( bounds );
-}
-
-int	AppCocoaTouch::getWindowHeight() const
-{
-	::CGRect bounds = [mState->mCinderView bounds];
-	return ::CGRectGetHeight( bounds );
-}
-
-//! Enables the accelerometer
-void AppCocoaTouch::enableAccelerometer( float updateFrequency, float filterFactor )
-{
-	mAccelFilterFactor = filterFactor;
+	{
+		mState = shared_ptr<AppCocoaTouchState>( new AppCocoaTouchState() );
+		mState->mStartTime = ::CFAbsoluteTimeGetCurrent();
+		mLastAccel = mLastRawAccel = Vec3f::zero();
+	}
 	
-	if( updateFrequency <= 0 )
-		updateFrequency = 30.0f;
+	void AppCocoaTouch::launch( const char *title, int argc, char * const argv[] )
+	{
+		::UIApplicationMain( argc, const_cast<char**>( argv ), nil, @"CinderAppDelegateIPhone" );
+	}
 	
-	[[UIAccelerometer sharedAccelerometer] setUpdateInterval:1.0 / updateFrequency];
-	CinderAppDelegateIPhone *appDel = (CinderAppDelegateIPhone *)[[UIApplication sharedApplication] delegate];
-	[[UIAccelerometer sharedAccelerometer] setDelegate:appDel];
-}
-
-void AppCocoaTouch::disableAccelerometer() {
+	int	AppCocoaTouch::getWindowWidth() const
+	{
+		::CGRect bounds = [mState->mCinderView bounds];
+		return ::CGRectGetWidth( bounds );
+	}
 	
-	[[UIAccelerometer sharedAccelerometer] setDelegate:nil];
-}
+	int	AppCocoaTouch::getWindowHeight() const
+	{
+		::CGRect bounds = [mState->mCinderView bounds];
+		return ::CGRectGetHeight( bounds );
+	}
+	
+	//! Enables the accelerometer
+	void AppCocoaTouch::enableAccelerometer( float updateFrequency, float filterFactor )
+	{
+		mAccelFilterFactor = filterFactor;
+		
+		if( updateFrequency <= 0 )
+			updateFrequency = 30.0f;
+		
+		[[UIAccelerometer sharedAccelerometer] setUpdateInterval:1.0 / updateFrequency];
+		CinderAccelerometerDelegateIPhone *appDel = [[CinderAccelerometerDelegateIPhone alloc] initWithApp:this];
+		[appDel retain];
+		[[UIAccelerometer sharedAccelerometer] setDelegate:appDel];
 
-//! Returns the maximum frame-rate the App will attempt to maintain.
-float AppCocoaTouch::getFrameRate() const
-{
-return 0;
-}
-
-//! Sets the maximum frame-rate the App will attempt to maintain.
-void AppCocoaTouch::setFrameRate( float aFrameRate )
-{
-}
-
-//! Returns whether the App is in full-screen mode or not.
-bool AppCocoaTouch::isFullScreen() const
-{
-return true;
-}
-
-//! Sets whether the active App is in full-screen mode based on \a fullScreen
-void AppCocoaTouch::setFullScreen( bool aFullScreen )
-{
-}
-
-double AppCocoaTouch::getElapsedSeconds() const
-{
-	CFAbsoluteTime currentTime = ::CFAbsoluteTimeGetCurrent();
-	return ( currentTime - mState->mStartTime );
-}
-
-std::string AppCocoaTouch::getAppPath()
-{ 
-	return [[[NSBundle mainBundle] bundlePath] UTF8String];
-}
-
-void AppCocoaTouch::quit()
-{
-return;
-}
-
-void AppCocoaTouch::privatePrepareSettings__()
-{
-	prepareSettings( &mSettings );
-}
-
-void AppCocoaTouch::privateTouchesBegan__( const TouchEvent &event )
-{
-	touchesBegan( event );
-}
-
-void AppCocoaTouch::privateTouchesMoved__( const TouchEvent &event )
-{
-	touchesMoved( event );
-}
-
-void AppCocoaTouch::privateTouchesEnded__( const TouchEvent &event )
-{
-	touchesEnded( event );
-}	
-
-void AppCocoaTouch::privateAccelerated__( const Vec3f &direction )
-{
-	Vec3f filtered = mLastAccel * (1.0f - mAccelFilterFactor) + direction * mAccelFilterFactor;
-	accelerated( AccelEvent( filtered, direction, mLastAccel, mLastRawAccel ) );
-	mLastAccel = filtered;
-	mLastRawAccel = direction;
-}
-
+	}
+	
+	void AppCocoaTouch::disableAccelerometer() {
+		
+		[[UIAccelerometer sharedAccelerometer] setDelegate:nil];
+	}
+	
+	//! Returns the maximum frame-rate the App will attempt to maintain.
+	float AppCocoaTouch::getFrameRate() const
+	{
+		return 0;
+	}
+	
+	//! Sets the maximum frame-rate the App will attempt to maintain.
+	void AppCocoaTouch::setFrameRate( float aFrameRate )
+	{
+	}
+	
+	//! Returns whether the App is in full-screen mode or not.
+	bool AppCocoaTouch::isFullScreen() const
+	{
+		return true;
+	}
+	
+	//! Sets whether the active App is in full-screen mode based on \a fullScreen
+	void AppCocoaTouch::setFullScreen( bool aFullScreen )
+	{
+	}
+	
+	double AppCocoaTouch::getElapsedSeconds() const
+	{
+		CFAbsoluteTime currentTime = ::CFAbsoluteTimeGetCurrent();
+		return ( currentTime - mState->mStartTime );
+	}
+	
+	std::string AppCocoaTouch::getAppPath()
+	{ 
+		return [[[NSBundle mainBundle] bundlePath] UTF8String];
+	}
+	
+	void AppCocoaTouch::quit()
+	{
+		return;
+	}
+	
+	void AppCocoaTouch::privatePrepareSettings__()
+	{
+		prepareSettings( &mSettings );
+	}
+	
+	void AppCocoaTouch::privateTouchesBegan__( const TouchEvent &event )
+	{
+		touchesBegan( event );
+	}
+	
+	void AppCocoaTouch::privateTouchesMoved__( const TouchEvent &event )
+	{
+		touchesMoved( event );
+	}
+	
+	void AppCocoaTouch::privateTouchesEnded__( const TouchEvent &event )
+	{
+		touchesEnded( event );
+	}	
+	
+	void AppCocoaTouch::privateAccelerated__( const Vec3f &direction )
+	{
+		Vec3f filtered = mLastAccel * (1.0f - mAccelFilterFactor) + direction * mAccelFilterFactor;
+		accelerated( AccelEvent( filtered, direction, mLastAccel, mLastRawAccel ) );
+		mLastAccel = filtered;
+		mLastRawAccel = direction;
+	}
+	
 } } // namespace cinder::app
